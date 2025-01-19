f536a92c74fd1a84c42b31fb1d3d24bc228fbd4a
Iman Namjouyan <im.namjuyan@gmail.com>
2025-01-19 12:58:01 +0330

    Update create-k8s-context.sh

--- create-k8s-context.sh
+++ create-k8s-context.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 BTOKEN=$(kubectl get secrets -n user1-ns user1-secret -oyaml |grep token|grep -v 'api'|grep -v type|awk {'print $2'})
+touch /tmp/user1_conf
 export KUBECONFIG=/tmp/user1_conf
 kubectl config set-cluster K8sTestServer --server https://192.168.1.100:6443 --certificate-authority=/etc/kubernetes/pki/ca.crt --embed-certs=true
 kubectl config set-credentials user1-sa --token=$BTOKEN
